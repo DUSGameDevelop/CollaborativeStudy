8ac2ed8896001c2a5cb1ef496afce3d156ffa1e7
DeadBodyJun <630cb@naver.com>
2025-08-05 21:21:56 +0900

    hello world
    hello world

---
+++ Assets/Script/practice.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class practice : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("hello world");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
